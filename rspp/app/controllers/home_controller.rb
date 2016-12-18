8873a074a85fbf398cb52a018b871c64d1938643
MikhailLU <clowfinger13@gmail.com>
2016-12-19 01:06:14 +0300

    first photo on cloudinary

--- rspp/app/controllers/home_controller.rb
+++ rspp/app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-
+      # Cloudinary::Uploader.upload('https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRFv1SeyEyQ11bW5AApV_wtTfZFQU-VQYrWMyESNfSVB6Y8kVEOqA')
   end
 end
