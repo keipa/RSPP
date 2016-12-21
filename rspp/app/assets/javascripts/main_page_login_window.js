3a0a24edc797ad674a1e72d9981fa4b38330e786
MikhailLU <clowfinger13@gmail.com>
2016-12-21 04:21:29 +0300

    scroll+footer

--- rspp/app/assets/javascripts/main_page_login_window.js
+++ rspp/app/assets/javascripts/main_page_login_window.js
@@ -1,31 +1,38 @@
-$(".login-btn").on("click", function(e) {
-  e.stopPropagation();
-  $(".user-actions").slideUp(250);
-  setTimeout(function() {
-    $(".login").slideDown(250);
-  }, 150);
-})
+$(document).ready(function() {
+    $(".login-btn").on("click", function(e) {
+        e.stopPropagation();
+        $(".user-actions").slideUp(250);
+        setTimeout(function() {
+            $(".login").slideDown(250);
+        }, 150);
+    })
 
-$(".signup-btn").on("click", function(e) {
-  e.stopPropagation();
-  $(".user-actions").slideUp(250);
-  setTimeout(function() {
-    $(".signup").slideDown(250);
-  }, 150);
-});
+    $(".signup-btn").on("click", function(e) {
+        e.stopPropagation();
+        $(".user-actions").slideUp(250);
+        setTimeout(function() {
+            $(".signup").slideDown(250);
+        }, 150);
+    });
 
-$(".wrapper").click(function(e) {
-  if (($(e.target).closest("form").hasClass("login-container")) || 
-      ($(e.target).closest("form").hasClass("signup-container"))) {
-    if (($(e.target)[0].type == "submit")) {
-      return true;
-    } else {
-      return false;
+    $("body").click(function(e) {
+        if (($(e.target).closest("form").hasClass("login-container")) ||
+            ($(e.target).closest("form").hasClass("signup-container"))) {
+            return $(e.target)[0].type === "submit" ? true : false;
+        }
+        $(".signup").slideUp(250);
+        $(".login").slideUp(250);
+        setTimeout(function() {
+            $(".user-actions").slideDown(250);
+        }, 150);
+    });
+
+    var maxNewsLength = 4;
+    addScroll($('.news-container'));
+
+    function addScroll(list) {
+        if (list.children().length > maxNewsLength && !list.hasClass('scroller')) {
+            list.addClass('scroller')
+        }
     }
-  }
-  $(".signup").slideUp(250);
-  $(".login").slideUp(250);
-  setTimeout(function() {
-    $(".user-actions").slideDown(250);
-  }, 150);
-});
+})
