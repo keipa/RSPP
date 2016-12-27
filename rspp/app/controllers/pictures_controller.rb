48309e45f892f9237ac75e52b6efdf01d6dce40b
MikhailLU <clowfinger13@gmail.com>
2016-12-27 19:51:13 +0300

    fix picture path

--- rspp/app/controllers/pictures_controller.rb
+++ rspp/app/controllers/pictures_controller.rb
@@ -1,4 +1,4 @@
-class ImagesController < ApplicationController
+class PicturesController < ApplicationController
   def index
       # Cloudinary::Uploader.upload('https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRFv1SeyEyQ11bW5AApV_wtTfZFQU-VQYrWMyESNfSVB6Y8kVEOqA')
   end
--- rspp/db/schema.rb
+++ rspp/db/schema.rb
@@ -15,6 +15,21 @@ ActiveRecord::Schema.define(version: 20161226152943) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "galeries", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "pictures", force: :cascade do |t|
+    t.string   "image"
+    t.string   "description"
+    t.integer  "gallery_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
   create_table "topics", force: :cascade do |t|
     t.string   "text",       null: false
     t.integer  "topic_id"
