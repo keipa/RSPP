fe8e3fb88639ba51487de3dec5e5beebae6e9562
MikhailLU <clowfinger13@gmail.com>
2017-01-20 00:30:04 +0300

    new picture load

--- rspp/app/controllers/news_posts_controller.rb
+++ rspp/app/controllers/news_posts_controller.rb
@@ -1,20 +1,20 @@
 class NewsPostsController < ApplicationController
-  def show
-    @post = NewsPost.find(params[:id])
-  end
+    def show
+        @post = NewsPost.find(params[:id])
+    end
 
-  def new
-    @post = NewsPost.new
-  end
+    def new
+        @post = NewsPost.new
+    end
 
-  def create
-    @post = NewsPost.create(news_post_params.merge(user_id: current_user.id))
-    redirect_to root_path
-  end
+    def create
+        @post = NewsPost.create(news_post_params.merge(user_id: current_user.id))
+        redirect_to root_path
+    end
 
-  private
+    private
 
-  def news_post_params
-    params.require(:news_post).permit(:title, :description, :text, :image_url)
-  end
+    def news_post_params
+        params.require(:news_post).permit(:title, :description, :text, :image_url)
+    end
 end
