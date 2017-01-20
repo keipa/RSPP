f47176e7fe4a7e09b1ba1c8b1b24bb71be455aaf
MikhailLU <clowfinger13@gmail.com>
2017-01-20 15:03:58 +0300

    statrting comment block, main page videos select

--- rspp/app/controllers/home_controller.rb
+++ rspp/app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
-  def index
-    @topics = Topic.all.where(subtopics: nil)
-    @news = NewsPost.all.reverse
-  end
+    def index
+        @topics = Topic.all.where(subtopics: nil)
+        @news = NewsPost.all.reverse
+        @videos = Video.all.where(type_video: 'interview').order('created_at desc').limit(5)
+    end
 end
