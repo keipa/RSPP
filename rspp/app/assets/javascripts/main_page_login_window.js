857f48e96ca85d2a51d007f531d98cf24ab19ee8
Nikita <nikita.kolokoltsev@yahoo.com>
2017-01-21 17:10:37 +0300

    Adaptive for tablets(landscape)

--- rspp/app/assets/javascripts/main_page_login_window.js
+++ rspp/app/assets/javascripts/main_page_login_window.js
@@ -1,18 +1,30 @@
 $(document).ready(function() {
     $(".login-btn").on("click", function(e) {
         e.stopPropagation();
-        $(".user-actions").slideUp(250);
-        setTimeout(function() {
-            $(".login-window").slideDown(250);
-        }, 150);
+        // Shadow is used only in adaptive mode
+        if ($(this).hasClass("shadow-on")) {
+            $(".shadow").fadeIn(250);
+            $(".login-window").fadeIn(450);
+        } else {
+            $(".user-actions").slideUp(250);
+            setTimeout(function() {
+                $(".login-window").slideDown(250);
+            }, 150);
+        }
     })
 
     $(".signup-btn").on("click", function(e) {
         e.stopPropagation();
-        $(".user-actions").slideUp(250);
-        setTimeout(function() {
-            $(".signup-window").slideDown(250);
-        }, 150);
+        // Shadow is used only in adaptive mode
+        if ($(this).hasClass("shadow-on")) {
+            $(".shadow").fadeIn(250);
+            $(".signup-window").fadeIn(450);
+        } else {
+            $(".user-actions").slideUp(250);
+            setTimeout(function() {
+                $(".signup-window").slideDown(250);
+            }, 150);
+        }
     });
 
     $("body").click(function(e) {
@@ -20,10 +32,21 @@ $(document).ready(function() {
             ($(e.target).closest("form").hasClass("signup-window-container"))) {
             return $(e.target)[0].type === "submit" ? true : false;
         }
-        $(".signup-window").slideUp(250);
-        $(".login-window").slideUp(250);
-        setTimeout(function() {
-            $(".user-actions").slideDown(250);
-        }, 150);
+
+        if ($(".vertical-navbar").is(":visible")) {
+
+            $(".shadow").fadeOut(250);
+            $(".login-window").fadeOut(250);
+            $(".signup-window").fadeOut(250);
+
+        } else {
+
+            $(".signup-window").slideUp(250);
+            $(".login-window").slideUp(250);
+            setTimeout(function() {
+                $(".user-actions").slideDown(250);
+            }, 150);
+
+        }
     });
 })
