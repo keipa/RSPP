57930509b68d3dafd6f9b227b687ccf6965fbbc0
Nikita <nikita.kolokoltsev@yahoo.com>
2017-01-28 01:05:50 +0300

    Fixed js

--- rspp/app/assets/javascripts/add_subtopics.js
+++ rspp/app/assets/javascripts/add_subtopics.js
@@ -1,33 +1,37 @@
-// $(document).on('turbolinks:load', function() {
+$(document).on('turbolinks:load', function() {
 
 
-//     var num = $(".numOfSubtopics")[1].value;
-//     $(".subtopicsInfo")[1].innerHTML = "";
-//     for (i = 0; i < num; i++) {
-//         $(".subtopicsInfo")[1].innerHTML += '<input placeholder="Название подраздела" name="topic[subtopics][' + i + '][text]">'+
-//                                             '<input placeholder="Ссылка на материал" name="topic[subtopics][' + i + '][link]">'+
-//                                             '<hr>';
-//     }
+    var num = $(".numOfSubtopics").val();
 
-//     $(".numOfSubtopics").on("change",function(){
-//         $(".subtopicsInfo")[1].innerHTML = "";
-//         for (i = 0; i < this.value; i++) {
-//             $(".subtopicsInfo")[1].innerHTML += '<input placeholder="Название подраздела" name="topic[subtopics][' + i + '][text]">'+
-//                                                 '<input placeholder="Ссылка на материал" name="topic[subtopics][' + i + '][link]">'+
-//                                                 '<hr>';
-//         }
-//     });
+    if (num) {
+      $(".subtopicsInfo")[1].innerHTML = "";
+      for (i = 0; i < num; i++) {
+          $(".subtopicsInfo")[1].innerHTML += '<input placeholder="Название подраздела" name="topic[subtopics][' + i + '][text]">'+
+                                              '<input placeholder="Ссылка на материал" name="topic[subtopics][' + i + '][link]">'+
+                                              '<hr>';
+      }
 
-//     $(".add-subtopic-sign").on("click", function(){
-//         topicID = $(this)[0].dataset.id;
-//         $(".add-subtopic-form").show();
+      $(".numOfSubtopics").on("change",function(){
+          $(".subtopicsInfo")[1].innerHTML = "";
+          for (i = 0; i < this.value; i++) {
+              $(".subtopicsInfo")[1].innerHTML += '<input placeholder="Название подраздела" name="topic[subtopics][' + i + '][text]">'+
+                                                  '<input placeholder="Ссылка на материал" name="topic[subtopics][' + i + '][link]">'+
+                                                  '<hr>';
+          }
+      });
 
-//         $("form").attr("action", "/topics/" + topicID);
-//         $("form").attr("method", "post");
+      $(".add-subtopic-sign").on("click", function(){
+          topicID = $(this)[0].dataset.id;
+          $(".add-subtopic-form").show();
 
-//     })
+          $("form").attr("action", "/topics/" + topicID);
+          $("form").attr("method", "post");
 
-//     $(".close").on("click", function() {
-//         $(".add-subtopic-form").hide();
-//     })
-// })
+      })
+
+      $(".close").on("click", function() {
+          $(".add-subtopic-form").hide();
+      })
+    }
+    
+})
