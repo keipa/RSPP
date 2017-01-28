7d61bbfb68f3843dfeacbb7fd59855ec5c4a659f
Nikita <nikita.kolokoltsev@yahoo.com>
2017-01-28 15:58:27 +0300

    News validations

--- rspp/db/migrate/20170119150939_create_news_posts.rb
+++ rspp/db/migrate/20170119150939_create_news_posts.rb
@@ -1,11 +1,13 @@
 class CreateNewsPosts < ActiveRecord::Migration[5.0]
   def change
     create_table :news_posts do |t|
-      t.string :title
+      t.string :title, null: false
       t.text :description
-      t.text :text
+      t.text :text, null: false
       t.string :image_url
       t.references :user, foreign_key: true
+
+      t.timestamps
     end
   end
 end
