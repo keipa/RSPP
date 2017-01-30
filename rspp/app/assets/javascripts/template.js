0d6dce98512dd863e20affe62b71ea4ed94eced9
Nikita <nikita.kolokoltsev@yahoo.com>
2017-01-31 02:36:51 +0300

    TOP TEMPLATES

--- rspp/app/assets/javascripts/template.js
+++ rspp/app/assets/javascripts/template.js
@@ -35,7 +35,9 @@ $(document).on('turbolinks:load', function() {
 	$('.update-description-topic').click(function() {
 		var wasHidden = $('.hidden-template');
 		var wasActive = $('.active-template');
-		wasActive.removeClass('active-template').addClass('hidden-template')
+		wasActive.remove();
+		//wasActive.removeClass('active-template').addClass('hidden-template')
 		wasHidden.removeClass('hidden-template').addClass('active-template')
 	})
+
 });
