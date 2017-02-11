01e6103cc2c64caa93cd2e8b884306e2ea3a09c3
MikhailLU <clowfinger13@gmail.com>
2017-02-12 00:09:32 +0300

    bill pdf

--- rspp/app/controllers/home_controller.rb
+++ rspp/app/controllers/home_controller.rb
@@ -27,13 +27,13 @@ class HomeController < ApplicationController
 		end
 
 		def bill
-      # respond_to do |format|
-      #   format.html
-      #   format.pdf do
-      #     pdf = WickedPdf.new.pdf_from_url("#{request.base_url}#{bill_path}")
-      #     send_data pdf
-      #   end
-      # end
+      respond_to do |format|
+        format.html
+        format.pdf do
+          pdf = WickedPdf.new.pdf_from_url("#{request.base_url}#{bill_path}")
+          send_data pdf
+        end
+      end
 		end
 
 		def statement
