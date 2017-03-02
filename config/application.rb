6ca5b95d45e7736c165cf59e33e12ad7806356bf
Nikita Kolokoltsev <nikita.kolokoltsev@yahoo.com>
2017-03-02 03:35:00 +0300

    ckeditor

--- config/application.rb
+++ config/application.rb
@@ -9,5 +9,6 @@ Bundler.require(*Rails.groups)
 module Rspp
   class Application < Rails::Application
     config.assets.precompile += Ckeditor.assets
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
   end
 end
