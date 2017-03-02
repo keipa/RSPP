2fc46af80900bb6396a23c1586ce7b4f01b022e6
Nikita Kolokoltsev <nikita.kolokoltsev@yahoo.com>
2017-03-02 03:10:34 +0300

    add topic maintenance

--- app/controllers/admin/topics_controller.rb
+++ app/controllers/admin/topics_controller.rb
@@ -6,31 +6,23 @@ class Admin::TopicsController < Admin::AdminController
   end
 
   def new
-    if params[:parent_topic_id]
-      @parent_topic = Topic.find(params[:parent_topic_id])
-    end
+    @parent_topic =
+      Topic.find(params[:parent_topic_id]) if params[:parent_topic_id]
     @topic = Topic.new
   end
 
   def create
-    @topic = Topic.create(
-      text: topic_params[:text],
-      smart_id: topic_params[:smart_id],
-      description: topic_params[:description],
-      topic_id: topic_params[:topic_id],
-      user_id: current_user.id
-    )
+    @topic = Topic.create(topic_params.merge(user_id: current_user.id))
     redirect_to admin_topics_path
   end
 
   def edit
-    if params[:parent_topic_id]
-      @parent_topic = Topic.find(params[:parent_topic_id])
-  	end
+    @parent_topic =
+      Topic.find(params[:parent_topic_id]) if params[:parent_topic_id]
   end
 
   def update
-    @topic.update_attributes(topic_params)
+    @topic.update(topic_params)
     redirect_to admin_topics_path
   end
 
@@ -52,5 +44,5 @@ class Admin::TopicsController < Admin::AdminController
       :topic_id,
       :description
     )
-    end
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -8,5 +8,6 @@ Bundler.require(*Rails.groups)
 
 module Rspp
   class Application < Rails::Application
+    config.assets.precompile += Ckeditor.assets
   end
 end
